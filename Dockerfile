d8ad6f7bdb056663c7cf85c41b847e32943d87b6
Michael Trang <michael.trang@hotmail.com>
2021-12-10 23:50:11 +1100

    Nestjs GraphQL API app with Nx

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM node:14-alpine
+
+WORKDIR /workspace
+
+COPY package.json /workspace/ 
+
+RUN npm i
+
+COPY  . . 
+
+RUN npm run build
+
+CMD ["npm","start"]
---
+++ apps/api/src/main.ts
@@ -0,0 +1,30 @@
+/**
+ * This is not a production server yet!
+ * This is only a minimal backend to get started.
+ */
+
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { NestFactory } from '@nestjs/core';
+
+import { AppModule } from './app/app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const config = app.get(ConfigService);
+  const globalPrefix = 'api';
+  app.setGlobalPrefix(globalPrefix);
+  const port = process.env.PORT || 3333;
+  await app.listen(port);
+  Logger.log(
+    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+  );
+  Logger.log(
+    `🚀 GraphQL Playground is running on: http://localhost:${port}/graphql`
+  );
+  Logger.log(
+    `🚀 Application is running in ${config.get('environment')} mode`
+  );
+}
+
+bootstrap();
